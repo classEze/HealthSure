1dc6547430053cfc99209a18e4f46b2b8c27c85d
Eze <60505216+classEze@users.noreply.github.com>
2021-10-09 02:03:58 +0100

    Update folio-message.js

--- pages/api/folio-message.js
+++ pages/api/folio-message.js
@@ -1,9 +1,18 @@
-export default function register_Folio(){
-  
-  
-  
-  
-  
-  
-  
-}
+import mongoose from 'mongoose'
+import connect_DB from 'top/db'
+import Message from 'top/Models/hospital'
+
+connect_DB(mongoose)
+
+export  default async (req, res) => {
+     if(req.body.sender && req.body.title && req.body.message ){
+          try{
+               await Message.create (req.body);
+               res.status(201).send('Message Received')
+             }
+       catch(err){
+         res.status(400).send(' OOps, We ran into some problem there!!')
+       }
+     }
+
+      }
