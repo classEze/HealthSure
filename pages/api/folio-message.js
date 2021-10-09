84f09c901dafcf77eeb30d8a7a47cb4b816959cf
Eze <60505216+classEze@users.noreply.github.com>
2021-10-09 03:42:11 +0100

    Update folio-message.js

--- pages/api/folio-message.js
+++ pages/api/folio-message.js
@@ -6,18 +6,19 @@ connect_DB(mongoose)
 
 export  default async (req, res) => {
     console.log(req.body)
-     if(req.body.sender && req.body.title && req.body.message ){
-          try{
-               await Message.create(req.body);
-               res.status(201).json({message:"Successfully saved"})
-             }
-       catch(err){
-         res.status(400).json({message:"Error, Message could not be saved"})
-       }
-     }
+    res.status(200).json)({message:"Successful"})
+//      if(req.body.sender && req.body.title && req.body.message ){
+//           try{
+//                await Message.create(req.body);
+//                res.status(201).json({message:"Successfully saved"})
+//              }
+//        catch(err){
+//          res.status(400).json({message:"Error, Message could not be saved"})
+//        }
+//      }
 
-     else{
-         res.status(400).json({message:'Useless request'})
+//      else{
+//          res.status(400).json({message:'Useless request'})
 
-     }
+//      }
      }
